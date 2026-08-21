67c220cda019faa6d42534c93cabf776c4d514c2
aauprightguy59-debug <aauprightguy59@gmail.com>
2026-08-21 13:41:54 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/EquiAi-Nexus-GBV-System/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
